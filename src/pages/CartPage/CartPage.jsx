7dbe51c001b3c6b3c8c7a67ab9aba9a02e444f03
Ahmed Maher <ahmedelseaidy22@gmail.com>
2024-07-30 18:08:56 +0300

    UPDATE: updating cart page.

---
+++ src/pages/CartPage/CartPage.jsx
@@ -0,0 +1,64 @@
+import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import CurrentUser from "../../models/CurrentUser";
+import { deleteCart, fetchUserCarts } from "../../services/carts";
+import CartComponent from "../../components/Cart/CartComponent/CartComponent";
+import CurrentCart from "../../models/CurrentCart";
+
+const CartPage = () => {
+  const user = new CurrentUser();
+  const cart = new CurrentCart();
+  const navigate = useNavigate();
+  const [pageData, setPageData] = useState({
+    carts: [],
+    err: null,
+    loadig: true,
+  });
+
+  const fetchCartData = () => {
+    fetchUserCarts(user.toObject().id)
+      .then((res) => {
+        setPageData((prev) => {
+          return { ...prev, carts: res.data.data, loadig: false };
+        });
+      })
+      .catch((err) => {
+        setPageData((prev) => {
+          return { ...prev, err, loadig: false };
+        });
+        console.log("Error retrieving carts: ", err);
+      });
+  };
+  const removeCart = async (id) => {
+    setPageData({ ...pageData, loadig: true });
+    await deleteCart(id).catch((err) => {
+      console.log(err);
+    });
+
+    cart.clearSession();
+    setPageData({ ...pageData, loadig: false });
+    fetchCartData();
+  };
+
+  useEffect(() => {
+    if (!user.sessionValid() && !user.isAdmin()) {
+      navigate("/home");
+      return;
+    }
+
+    fetchCartData();
+  });
+
+  return (
+    <div className="cart-page">
+      {pageData.carts &&
+        pageData.carts.map((cart, index) => {
+          return (
+            <CartComponent key={index} cart={cart} onClickEvent={removeCart} />
+          );
+        })}
+    </div>
+  );
+};
+
+export default CartPage;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import Layout from "./layout/Layout";
 import Home from "./pages/Home/Home";
-import Cart from "./pages/Cart/Cart";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Logout from "./components/Logout/Logout";
 import Products from "./pages/Products/Products";
+import CartPage from "./pages/CartPage/CartPage";
 import OrderPage from "./pages/OrderPage/OrderPage";
 import { createHashRouter } from "react-router-dom";
 import UsersPage from "./pages/UsersPage/UsersPage";
@@ -18,7 +18,7 @@ const router = createHashRouter([
     element: <Layout />,
     children: [
       { path: "", element: <Home /> },
-      { path: "/cart", element: <Cart /> },
+      { path: "/cart", element: <CartPage /> },
       { path: "/home", element: <Home /> },
       { path: "/orders", element: <OrderPage /> },
       { path: "/products", element: <Products /> },
